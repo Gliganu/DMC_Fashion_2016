bc9113723a6c5585b180f15a6c0f0728e74b3a91
Bogdan-Morosanu <bogdan.moro.mail@gmail.com>
2016-04-18 21:54:22 +0300

    changed \ in paths to / which works on all systems

--- zaCode/FileManager.py
+++ zaCode/FileManager.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 
 def getWholeTrainingData():
-    inputFileName = '..\\data\\orders_train.txt'
+    inputFileName = '../data/orders_train.txt'
     data = pd.read_csv(inputFileName, delimiter=';', skipinitialspace=True)
 
     return data
 
 
 def getTestData():
-    inputFileName = '..\\data\\orders_class.txt'
+    inputFileName = '../data/orders_class.txt'
     data = pd.read_csv(inputFileName, delimiter=';', skipinitialspace=True)
 
     return data
@@ -17,28 +17,28 @@ def getTestData():
 
 # added these 'peasanty' functions becauuse although we could've read the whole csv and trim the data frame after, it's faster this way
 def get1kTrainingData():
-    inputFileName = '..\\data\\orders_train_1k.txt'
+    inputFileName = '../data/orders_train_1k.txt'
     data = pd.read_csv(inputFileName, delimiter=';', skipinitialspace=True)
 
     return data
 
 
 def get10kTrainingData():
-    inputFileName = '..\\data\\orders_train_10k.txt'
+    inputFileName = '../data/orders_train_10k.txt'
     data = pd.read_csv(inputFileName, delimiter=';', skipinitialspace=True)
 
     return data
 
 
 def get100kTrainingData():
-    inputFileName = '..\\data\\orders_train_100k.txt'
+    inputFileName = '../data/orders_train_100k.txt'
     data = pd.read_csv(inputFileName, delimiter=';', skipinitialspace=True)
 
     return data
 
 
 def get1000kTrainingData():
-    inputFileName = '..\\data\\orders_train_1000k.txt'
+    inputFileName = '../data/orders_train_1000k.txt'
     data = pd.read_csv(inputFileName, delimiter=';', skipinitialspace=True)
 
     return data
